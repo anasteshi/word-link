4e9100d51216c79c20038d8d22dd86121fd6c295
Anastasiia <anasteshi13@gmail.com>
2025-08-02 12:53:05 +0300

    Write letters with keyboard

--- main.js
+++ main.js
@@ -3,24 +3,20 @@ import WordBuilder from "./wordBuilder.js"
 const canvas = document.getElementById("game-canvas")
 const wordBuilder = new WordBuilder(canvas)
 
-wordBuilder.addLetter("A")
-wordBuilder.addLetter("B")
-wordBuilder.addLetter("C")
+window.addEventListener("resize", () => {
+	wordBuilder.resize()
+})
+
+window.addEventListener("keydown", (e) => {
+	if (e.key.length === 1 && /^[a-zA-Z]$/.test(e.key)) { // Regex to check if the key is an english letter
+		wordBuilder.addLetter(e.key.toUpperCase())
+	}
+})
 
 function gameLoop() {
 	wordBuilder.update()
 	wordBuilder.draw()
 	requestAnimationFrame(gameLoop)
 }
-
-window.addEventListener("resize", () => {
-	wordBuilder.resize()
-})
-
-window.addEventListener("keydown", (e) => {
-	if (e.key.length === 1 && /^[a-zA-Z]$/.test(e.key)) {
-		wordBuilder.addLetter(e.key.toUpperCase())
-	}
-})
 
 gameLoop()
