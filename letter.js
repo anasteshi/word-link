b2c4121f9e847255c36e4daf9287a50df9d06b05
Anastasiia <anasteshi13@gmail.com>
2025-08-02 14:30:57 +0300

    Add repulsion and friction

--- letter.js
+++ letter.js
@@ -22,7 +22,25 @@ export default class Letter {
 			this.vy *= -0.8
 		}
 
+		for (const otherLetter of letters) {
+			if (otherLetter == this) {
+				continue
+			}
 
+			const dx = this.x - otherLetter.x
+			const dy = this.y - otherLetter.y
+			const distance = Math.hypot(dx, dy)
+
+			if (distance < 40) {
+				const force = (40 - distance) * 0.05
+
+				this.vx += (dx / distance) * force
+				this.vy += (dy / distance) * force
+			}
+		}
+
+		this.vx *= 0.98
+		this.vy *= 0.98
 	}
 
 	draw(ctx) {
