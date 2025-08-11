6e3bc17f4c90b7b79d94360e45bfba290ec3d21c
Anastasiia <anasteshi13@gmail.com>
2025-08-11 23:49:34 +0300

    Show 'start typing' hint

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ import WordBuilder from "./wordBuilder.js"
 
 const canvas = document.getElementById("game-canvas")
 const wordBuilder = new WordBuilder(canvas)
+const message = document.getElementById("message")
 
 window.addEventListener("resize", () => {
 	wordBuilder.resize()
 })
 
 window.addEventListener("keydown", (e) => {
+	if (message.classList.contains("fade-message")) {
+		message.classList.add("hidden")
+	}
+
 	if (e.key.length === 1 && /^[a-zA-Z]$/.test(e.key)) {
 		// Regex to check if the key is an english letter
 		wordBuilder.addLetter(e.key.toUpperCase())
